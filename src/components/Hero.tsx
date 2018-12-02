e134019bef72497a64e887d66e4c340a3e42a723
chayeoi <chayeoikeem@gmail.com>
2018-12-02 15:39:26 +0900

    refactor: Inject background-color from theme

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,28 +3,23 @@ import { mainPhoto } from '../../static/images'
 
 const Hero = styled.div`
   position: relative;
-  height: 3333px;
   &::before {
     content: '';
     position: absolute;
     top: 0;
     left: 0;
-    z-index: -1000;
+    z-index: -1;
     width: 100%;
     height: 100%;
-    background: #7c72eb;
+    background: ${({ theme }) => theme.backgroundColor} url(${mainPhoto}) center top / 150% no-repeat;
+    filter: blur(2px);
   }
   &::after {
     content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1000;
-    opacity: .8;
-    width: 100%;
-    height: 100%;
-    background: url(${mainPhoto}) center top / 150% no-repeat;
-    filter: blur(2px);
+    display: block;
+    width: 100px;
+    height: 100px;
+    background-color: red;
   }
 `
 
