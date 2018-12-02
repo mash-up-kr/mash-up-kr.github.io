cb8e0f7bd2d7ed5b3418ddab37b8ecf0bf024cf9
chayeoi <chayeoikeem@gmail.com>
2018-12-02 22:07:50 +0900

    feat: Add background image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components'
-import { mainPhoto } from '../assets/images'
+import { mainPhoto, mobileBg } from '../assets/images'
 
 const Hero = styled.div`
   position: relative;
+  background: ${({ theme }) => theme.backgroundColor} url(${mainPhoto}) center top / 150% no-repeat;
   &::before {
     content: '';
     position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
+    top: 40%;
     width: 100%;
-    height: 100%;
-    background: ${({ theme }) => theme.backgroundColor} url(${mainPhoto}) center top / 150% no-repeat;
-    filter: blur(2px);
+    height: 1500px;
+    background: url(${mobileBg}) no-repeat;
+    background-size: cover;
+  }
+  & > * {
+    position: relative;
   }
 `
 
