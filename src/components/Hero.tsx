44a0cf62dc761285cb8036b64f1883a618fbb17c
chayeoi <chayeoikeem@gmail.com>
2018-12-24 01:34:43 +0900

    feat: Apply linear-gradient to 'Hero'

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,8 @@ const Hero = styled.div`
   position: relative;
   background:
     url(${backgroundImage}) 0 -250px / cover no-repeat,
-    ${({ theme }) => theme.backgroundColor} url(${mainPhoto}) center top / 150% no-repeat;
+    url(${mainPhoto}) center top / 150% no-repeat,
+    ${({ theme }) => theme.backgroundColor} linear-gradient(to bottom, #7C72EB, #7C72EB 88%, #4C3599 100%) no-repeat;
 `
 
 export default Hero
